8fe7c5901d40249befcdafa8e14671a898bcef9d
lbrok <swedishnrg@gmail.com>
2022-11-29 14:47:04 +0100

    Plots & generated data

--- scripts/gc_distribution_plots.py
+++ scripts/gc_distribution_plots.py
@@ -28,13 +28,19 @@ def stdCalc(data):
     std_GC = variance**0.5
     return std_GC
 
-def findOutliers(data, mean, std):
+def findOutliers(data, mean, std, minNumWindows=False):
     data['Outlier'] = ""
     for i in range(len(data)):
-        if i < round(mean-numOfStds*std) or i > round(mean+numOfStds*std):
-            data.loc[i,'Outlier'] = True
+        if minNumWindows != False:
+            if data.loc[i,'NumOfWindows'] < minNumWindows:
+                data.loc[i,'Outlier'] = True
+            else:
+                data.loc[i,'Outlier'] = False
         else:
-            data.loc[i,'Outlier'] = False
+                if i < round(mean-numOfStds*std) or i > round(mean+numOfStds*std):
+                    data.loc[i,'Outlier'] = True
+                else:
+                    data.loc[i,'Outlier'] = False
 
 def dataFrameToList(data, column):
     lst = data[column].where(data['Outlier']==False)
@@ -52,33 +58,29 @@ numOfStds = 3
 data = readFiles(file_path)
 mean_GC = meanCalc(data)
 std_GC = stdCalc(data)
-findOutliers(data, mean_GC, std_GC)
+findOutliers(data, mean_GC, std_GC, 1000000/window_size)
 
 removedOutliers = sum(data.loc[data['Outlier'], 'NumOfWindows'].tolist())
 totalWindows = data['NumOfWindows'].sum()
 removedOutliersFraction = 100*(removedOutliers/totalWindows)
 
 plt.subplot(2,1,1)
-plt.bar(data['GC%'], data['NumOfWindows'],color=plot_color)
-plt.bar(data['GC%'].where(data['Outlier']),data['NumOfWindows'],color='white', edgecolor='grey')
+plt.bar(data['GC%'], data['NumOfWindows'],color=plot_color, label='Included GC%')
+plt.bar(data['GC%'].where(data['Outlier']),data['NumOfWindows'],color='white', edgecolor='grey', label='Excluded GC%')
 plt.axvline(x=mean_GC, color='black')
-plt.axvline(x=round(mean_GC-numOfStds*std_GC)-0.5, color = 'grey')
-plt.axvline(x=round(mean_GC+numOfStds*std_GC)+0.5, color = 'grey')
+plt.axhline(y=1000000/window_size, color='grey')
 plt.text(mean_GC*1.01,max(data['NumOfWindows'])+max(data['NumOfWindows'])*0.001,f'Mean: {round(mean_GC,2)}')
-plt.text((mean_GC+numOfStds*std_GC)*1.01,max(data['NumOfWindows'])*0.15,f'3 std confidence: {round(mean_GC,2)}±{round(numOfStds*std_GC,2)} GC% \nWindows removed: {round(removedOutliersFraction,2)}% ({removedOutliers}/{totalWindows})')
+plt.text(max(data.index[data['Outlier']==False].tolist())*1.01,(1000000/window_size)*1.15,f'Min quantity threshold: {int(1000000/window_size)} \nWindows removed: {round(removedOutliersFraction,3)}% ({removedOutliers}/{totalWindows})')
 plt.xlabel('GC-content [%]')
 plt.ylabel('Quantity')
 plt.title(f'{sample}, window size {window_size} bp')
 plt.xlim([-5,105])
-#plt.yscale('log')
+plt.legend(fontsize=8)
+plt.yscale('log')
 
 plt.subplot(2,1,2)
 plt.errorbar(data['GC%'].where(data['Outlier'] == False),data['Mean'], yerr=data['Std'],color=plot_color, ecolor=plot_color, fmt='o',zorder=1)
 plt.errorbar(data['GC%'].where(data['Outlier']),data['Mean'], yerr=data['Std'], fmt='wo', ms=7, mec='grey',ecolor='grey',zorder=2)
-plt.axvline(x=round(mean_GC-numOfStds*std_GC)-0.5, color = 'grey')
-plt.axvline(x=round(mean_GC+numOfStds*std_GC)+0.5, color = 'grey')
-plt.text(mean_GC*1.01,max(data['NumOfWindows'])+max(data['NumOfWindows'])*0.005,f'Mean: {round(mean_GC,2)}')
-plt.text((mean_GC+numOfStds*std_GC)*1.01,max(data['NumOfWindows'])*0.15,f'99% confidence: {round(mean_GC,2)}+-{round(numOfStds*std_GC,2)}')
 
 mean_list = dataFrameToList(data,'Mean')
 GC_list = dataFrameToList(data, 'GC%')
@@ -89,7 +91,7 @@ y_bar = np.mean(mean_list) # Mean
 R2 = np.sum((y_model - y_bar) ** 2) / np.sum((mean_list - y_bar) ** 2) # Coefficient of determination, R²
 xlim = plt.xlim()
 #Ploting the linear regression
-plt.plot(np.array(xlim), p[1] + p[0] * np.array(xlim), '--', linewidth=1.2, label=f'Line of Best Fit, R² = {R2:.2f}', color='black', zorder=3)
+plt.plot(np.array(xlim), p[1] + p[0] * np.array(xlim), '--', linewidth=1.2, label=f'Line of Best Fit, R² = {R2:.2f}, y={round(p[0],4)}*x{round(p[1],4)}', color='black', zorder=3)
 plt.legend(fontsize=8)
 plt.axhline(y=0, color='black', linewidth=1)
 plt.xlim([-5,105])
