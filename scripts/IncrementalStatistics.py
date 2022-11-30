d2a0cd2c52426ef67d210b54e4a8c6c787cb4cdb
lbrok <swedishnrg@gmail.com>
2022-11-30 09:07:26 +0100

    New telomere lengths

--- scripts/IncrementalStatistics.py
+++ scripts/IncrementalStatistics.py
@@ -2,9 +2,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-telomeres = [3037418,3037418,0,3037418,3037418,3037418,0,3037418,3037418,3037418,3037418,3037418,3037418,3037418,3037418,3037418,3037418,3037418,3037418,3037418,0,3037418,3037418,3037418,3037418,3037418,3037418,3037418]
-#names = ['chr1', 'chr2', 'chr3', 'chr4', 'chr5', 'chr6', 'chr7', 'chr8', 'chr9', 'chr10', 'chr11', 'chr12', 'chr13', 'chr14', 'chr15', 'chr16', 'chr17', 'chr18', 'chr19', 'chr20', 'chr21', 'chr22', 'chr23', 'chr24', 'chr25', 'chr26', 'chr27', 'chrX']
-names = ['chr24']
+telomeres = [3000000,3000000,0,3000000,3000000,3000000,0,3000000,3000000,3000000,3000000,3000000,3000000,3000000,300000,3000000,3000000,3000000,3000000,3000000,0,3000000,3000000,3000000,3000000,3000000,3000000,3000000]
+names = ['chr1', 'chr2', 'chr3', 'chr4', 'chr5', 'chr6', 'chr7', 'chr8', 'chr9', 'chr10', 'chr11', 'chr12', 'chr13', 'chr14', 'chr15', 'chr16', 'chr17', 'chr18', 'chr19', 'chr20', 'chr21', 'chr22', 'chr23', 'chr24', 'chr25', 'chr26', 'chr27', 'chrX']
+#names = ['chr24']
 path = 'D:/Data/mammoth.'
 
 '''
@@ -20,7 +20,7 @@ def incrementalStatistics(filePath, names, telomeres):
     #meanValue = 33.108845127365235 #Used when removing outliers, this is the previous average
     #stdValue = 10.777236719119456 #Used when removing outliers, this is the previous average
     for i in range(len(names)):
-        for chunk in pd.read_csv(filePath+names[i]+'.depth', sep='\t', comment='t', header=None, usecols=[2,3,4,5], skiprows=telomeres[i], chunksize=10000000):
+        for chunk in pd.read_csv(filePath+names[i]+'.depth', sep='\t', comment='t', header=None, usecols=[6,7,8,9], skiprows=telomeres[i], chunksize=5000000):
             chunk = chunk.mean(axis=1) #Calculates the mean value of each input row
             for index, mammothMean in chunk.items(): #Iterates through each row
                 #if mammothMean < meanValue + 2*stdValue: #Add / remove if statement if outliers should be removed or not
